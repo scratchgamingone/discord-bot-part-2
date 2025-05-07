1026778576edab7659fab9d35274fd6bed0c626c
scratchgamingone <96272119+scratchgamingone@users.noreply.github.com>
2025-05-07 11:03:02 -0400

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -1,23 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
-
 module.exports = {
     name: 'av',
     description: 'Shows your avatar or someone else\'s avatar',
     execute(message, args) {
-        // Get the mentioned user or default to the message author
-        const user = message.mentions.users.first() || message.author;
+        // Check if the message is exactly !av or starts with !av + mention
+        if (message.content.startsWith('!av')) {
+            const user = message.mentions.users.first() || message.author;
+            const avatarUrl = user.displayAvatarURL({ dynamic: true, size: 512 });
 
-        // Get the avatar URL
-        const avatarUrl = user.displayAvatarURL({ dynamic: true, size: 512 });
+            const { EmbedBuilder } = require('discord.js');
+            const embed = new EmbedBuilder()
+                .setTitle(`@${user.username}`)
+                .setImage(avatarUrl)
+                .setColor(0xff0000)
+                .setFooter({ text: `Requested by ${message.author.tag}` });
 
-        // Send the avatar in an embed
-        message.channel.send({
-            embeds: [{
-                color: 0x0099ff,
-                title: `${user.username}'s Avatar`,
-                image: { url: avatarUrl },
-                footer: { text: `Requested by ${message.author.tag}` }
-            }]
-        });
+            message.channel.send({ embeds: [embed] });
+        }
     }
 };
